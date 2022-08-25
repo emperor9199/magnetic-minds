818bee6497837c84807b8bf61b0ad90488570bc8
emperor9199 <199emperor@gmail.com>
2022-08-26 04:47:50 +0530

    Navbar is transparent now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer/Footer";
+import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
+// import "antd/dist/antd.css";
 
 const App = () => {
   return (
     <div>
+      <HeroSection />
       <Header />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 };
